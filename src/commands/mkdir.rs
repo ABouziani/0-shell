4826aec15b702a8ee9519bdeece9156fc22a7c18
otmane chouari <otmanchouarix3@gmail.com>
2025-07-12 08:10:47 +0100

    fixes

--- src/commands/mkdir.rs
+++ src/commands/mkdir.rs
@@ -1,7 +1,6 @@
 use std::fs;
-use std::io::{self, Write};
 
-fn mkdir(args: &[String]) {
+pub fn mkdir(args: &[String]) {
     if args.is_empty() {
         eprintln!("mkdir: missing operand");
         return;
@@ -10,9 +9,6 @@ fn mkdir(args: &[String]) {
     for path in args {
         match fs::create_dir(path) {
             Ok(_) => {}
-            Err(ref e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
-                eprintln!("mkdir: cannot create directory '{}': File exists", path);
-            }
             Err(e) => {
                 eprintln!("mkdir: cannot create directory '{}': {}", path, e);
             }
