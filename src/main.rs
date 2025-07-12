d6cea046f82e947e7bf18dca6aefaa883305ddf9
otmane chouari <otmanchouarix3@gmail.com>
2025-07-12 08:06:44 +0100

    session one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::io::{self, Write};
+use std::process;
+
+mod commands; 
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        let bytes = io::stdin().read_line(&mut input);
+
+        //  ctr+ d (EOF)
+        match bytes {
+            Ok(0) => {
+                println!();
+                break;
+            }
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                continue;
+            }
+        }
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        let parts: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
+        let command = &parts[0];
+        let args = &parts[1..];
+        //commmands match
+        match command.as_str() {
+            "mkdir" => commands::mkdir::mkdir(args),
+            "exit" => {
+                println!("Bye!");
+                process::exit(0);
+            }
+            _ => eprintln!("Command '{}' not found", command),
+        }
+    }
 }
