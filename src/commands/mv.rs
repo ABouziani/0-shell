9da5d3eb678ac5cc4e0b809ae3afc95620258f83
Abdelhamid Bouziani <abouziani>
2025-07-20 15:30:03 +0100

    fixing mv command

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -4,25 +4,32 @@ use std::fs::File;
 
 use std::path::Path;
 pub fn mv(args: &[&str]) {
-    match std::env::current_dir() {
-        Ok(path) => println!("{}", path.display()),
-        Err(e) => eprintln!("pwd: {}", e),
-    }
     if args.len() < 2 {
         println!("Error: argement in valid");
         return;
     }
-    let new_path = args[args.len() - 1];
-    for i in args {
+    let path = args[args.len() - 1];
+    for i in &args[0..args.len() - 1] {
         let old_path = Path::new(i);
-        println!("{:#?}",old_path);
-        let new_path = Path::new(&new_path);
-        if !old_path.exists() || !new_path.exists() {
-            println!("path in valid",); 
+        let new_path = Path::new(&path);
+        if path.contains("/") && !new_path.exists() {
+            println!("mv: cannot move {:?} to {:?}: Not a directory",old_path,new_path);
+            continue;
+        }
+        if !old_path.exists(){
+            println!("mv: cannot stat {:?}: No such file or directory",old_path);
             continue;
         }
         if new_path.is_dir() && old_path.is_file() {
-            let v = new_path.join(old_path);
+            let name_file = old_path.file_name();
+            let t = match name_file {
+                Some(name) => name,
+                None => {
+                    eprintln!("Error: Invalid source file name.");
+                    continue;
+                }
+            };
+            let v = new_path.join(t);
 
             let _ = match File::create(&v) {
                 Ok(v) => v,
@@ -32,11 +39,11 @@ pub fn mv(args: &[&str]) {
                 }
             };
 
-            _ = fs::copy(old_path, new_path);
+            _ = fs::copy(v, new_path);
 
             _ = fs::remove_file(old_path);
         } else {
-           _= fs::rename(old_path, new_path);
+            _ = fs::rename(old_path, new_path);
         }
     }
 }
