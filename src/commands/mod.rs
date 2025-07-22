d65c64d31431d092dd9881a14a1ab6bdcc0ae63a
x3alone <otmanchouarix3@gmail.com>
2025-07-22 10:11:01 +0100

    working echo removed the bonus

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,1 +1,2 @@
  pub mod mkdir;
+ pub mod echo;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Write};
 use std::process;
 
-mod commands; 
+mod commands;
 
 fn main() {
     loop {
@@ -11,7 +11,7 @@ fn main() {
         let mut input = String::new();
         let bytes = io::stdin().read_line(&mut input);
 
-        //  ctr+ d (EOF)
+        // Handle EOF (Ctrl+D)
         match bytes {
             Ok(0) => {
                 println!();
@@ -23,6 +23,7 @@ fn main() {
                 continue;
             }
         }
+
         let input = input.trim();
         if input.is_empty() {
             continue;
@@ -31,9 +32,11 @@ fn main() {
         let parts: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
         let command = &parts[0];
         let args = &parts[1..];
-        //commmands match
+
+        // Command match
         match command.as_str() {
             "mkdir" => commands::mkdir::mkdir(args),
+            "echo" => commands::echo::echo(args),
             "exit" => {
                 println!("Bye!");
                 process::exit(0);
