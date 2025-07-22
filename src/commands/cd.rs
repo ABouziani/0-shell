79e8a20020fbf079c6364ab811ca33ff42db8ed2
x3alone <otmanchouarix3@gmail.com>
2025-07-22 17:50:44 +0100

    cd fixed cases

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,19 +1,61 @@
 use std::env;
+use std::path::PathBuf;
+use std::sync::Mutex;
+
+static mut PREV_DIR: Option<Mutex<PathBuf>> = None;
 
 pub fn cd(args: &Vec<&str>) {
-    let target = if args.is_empty() { 
-        match env::var("HOME") {
-            Ok(path) => path,
-            Err(_) => {
-                eprintln!("cd: HOME not set");
+    let home_dir = match env::var("HOME") {
+        Ok(path) => path,
+        Err(_) => {
+            eprintln!("cd: HOME not set");
+            return;
+        }
+    };
+
+    let current_dir = match env::current_dir() {
+        Ok(dir) => dir,
+        Err(_) => PathBuf::from("/"),
+    };
+
+    unsafe {
+        if PREV_DIR.is_none() {
+            PREV_DIR = Some(Mutex::new(current_dir.clone()));
+        }
+    }
+
+    let target = if args.is_empty() {
+        home_dir.clone()
+    } else if args[0] == "-" {
+        unsafe {
+            if let Some(ref prev_mutex) = PREV_DIR {
+                let mut prev = prev_mutex.lock().unwrap();
+                println!("{}", prev.display());
+                std::mem::replace(&mut *prev, current_dir.clone()).to_string_lossy().to_string()
+            } else {
+                eprintln!("cd: OLDPWD not set");
                 return;
             }
         }
     } else {
-        args[0].to_string().clone()  
+        let input = args[0];
+        if input == "$HOME" {
+            home_dir.clone()
+        } else if input.starts_with("~") {
+            input.replacen("~", &home_dir, 1)
+        } else {
+            input.to_string()
+        }
     };
 
     if let Err(e) = env::set_current_dir(&target) {
         eprintln!("cd: {}: {}", target, e);
+    } else {
+        unsafe {
+            if let Some(ref prev_mutex) = PREV_DIR {
+                let mut prev = prev_mutex.lock().unwrap();
+                *prev = current_dir;
+            }
+        }
     }
 }
